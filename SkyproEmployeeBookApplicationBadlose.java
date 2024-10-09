b2bc8f56122c4f9beac41a697c13361ff806ea27
Badlose <git@github.com:Badlose/helloworld.git>
2024-10-09 16:12:35 +0300

    Initial commit

---
+++ SkyproEmployeeBookApplicationBadlose.java
@@ -0,0 +1,13 @@
+package pro.sky.skyproemployeebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproEmployeeBookApplicationBadlose {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproEmployeeBookApplicationBadlose.class, args);
+	}
+
+}
